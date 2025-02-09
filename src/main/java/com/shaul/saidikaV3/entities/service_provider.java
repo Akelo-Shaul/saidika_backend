eb3205cbd39bbf8a3ddd166c8441f11ec11ffaa4
Akelo-Shaul <shaulakelo@gmail.com>
2025-02-09 18:45:20 +0300

    provider changes

--- src/main/java/com/shaul/saidikaV3/entities/service_provider.java
+++ src/main/java/com/shaul/saidikaV3/entities/service_provider.java
@@ -1,5 +1,6 @@
 package com.shaul.saidikaV3.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
@@ -19,6 +20,7 @@ public class service_provider extends Users{
 
     @OneToMany(mappedBy = "provider",fetch= FetchType.LAZY)
     @JsonProperty("os")
+    @JsonIgnore
     private List<offered_services> offeredServices;
 
 
