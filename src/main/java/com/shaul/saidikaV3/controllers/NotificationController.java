7b261e146641967ced1c4144761480d1655dc9db
Akelo-Shaul <shaulakelo@gmail.com>
2025-02-19 15:10:04 +0300

    Notification added working...not sure

--- src/main/java/com/shaul/saidikaV3/controllers/NotificationController.java
+++ src/main/java/com/shaul/saidikaV3/controllers/NotificationController.java
@@ -1,15 +1,19 @@
 package com.shaul.saidikaV3.controllers;
 
+import com.shaul.saidikaV3.entities.Users;
+import com.shaul.saidikaV3.entities.service_provider;
 import com.shaul.saidikaV3.requestModels.NotificationRequest;
 import com.shaul.saidikaV3.responsemodels.NotificationResponse;
 import com.shaul.saidikaV3.services.FCMService;
+import com.shaul.saidikaV3.services.service_finder_service;
+import com.shaul.saidikaV3.services.service_provider_service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -17,12 +21,29 @@ public class NotificationController {
 
     @Autowired
     private FCMService fcmService;
-
+    @Autowired
+    service_provider_service provider_service;
+    @Autowired
+    service_finder_service finder_service;
 
     @PostMapping("/notification")
     public ResponseEntity sendNotification(@RequestBody NotificationRequest request) throws ExecutionException, InterruptedException, ExecutionException {
         fcmService.sendMessageToToken(request);
         return new ResponseEntity<>(new NotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
     }
+
+    @GetMapping("/getNotificationToken")
+    public String getNotificationTok(@RequestParam("ReceiverID") UUID id) {
+        String token;
+        if(provider_service.find_by_id(id).isPresent()){
+            token=provider_service.find_by_id(id).get().getNotifToken();
+        } else{
+            token = provider_service.find_by_id(id).get().getNotifToken();
+        }
+        return token;
+    }
+
+
+
 
 }
